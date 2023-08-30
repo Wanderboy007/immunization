12c75eeabc29cb91c608208f8da970ed2fadd099
tejas <ctejas44@gmail.com>
2023-08-30 23:21:06 +0530

    login page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const login = () => {
-  return <div>this login</div>;
+  return (
+    <>
+      <h1>Login</h1>
+      <div>
+        <label htmlFor="username">username </label>
+        <input type="text" name="username" className="" />
+      </div>
+      <div>
+        <label htmlFor="password">password </label>
+        <input type="password" name="password" className="" />
+      </div>
+      <div>
+        <a href="#">forgot password</a>
+      </div>
+      <div>
+        <input type="button" value="login" />
+      </div>
+    </>
+  );
 };
 
 export default login;
