635c001e58f5425dd17a9e32d2c056c32b33ad96
ShivamxPawale <saheyp6@gmail.com>
2023-12-25 23:59:42 +0530

    analytic page design

--- src/pages/Analytics/Analytic.jsx
+++ src/pages/Analytics/Analytic.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
+import "./analytic.css";
 
 const Analytic = () => {
-  return <div></div>;
+  return (
+    <>
+    
+    <div className="vaccine">
+    <Navbar/>
+      <div className="top-4">
+     <div className="registration-block">
+       <h6>Registration</h6>
+     </div>
+     <div className="sex-block">
+     <h6>Sex</h6>
+     </div>
+     <div className="age-block">
+     <h6>Age</h6>
+     </div>
+     <div className="wght-block">
+     <h6>weight and height</h6>
+     </div>
+     </div>
+
+
+<div className="bottom-2">
+     <div className="vaccine-block">
+     <h6>Vaccine</h6>
+     </div>
+     <div className="immunized-block">
+     <h6>Immunization</h6>
+     </div>
+     </div>
+
+     </div>
+    </>
+  );
 };
 
 export default Analytic;
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -5,10 +5,10 @@ import "./navbar.css";
 const Navbar = () => {
   return (
     <>
-      <nav>
+      <nav className="navbar-bg">
         <ul className="navbar_ul">
-          <li className="navbar_li">
-            <Link to="/dashbord">Dashbord</Link>
+          <li className="navbar_li" >
+            <Link to="/dashbord">Dashboard</Link>
           </li>
           <li className="navbar_li">
             <Link to="/analytic">Analytic</Link>
